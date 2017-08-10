2bd2dd05d771cb3cc97c024443c0dcd3f7283553
David Nishikawa <triptype@gmail.com>
2017-08-10 10:26:50 -0700

    created buttons dynamically

--- maps.js
+++ maps.js
@@ -1,1 +1,25 @@
-alert('js online');
+
+
+console.log(document.getElementById('location-nav'));
+
+
+var mapBank = {
+    elNav: document.getElementById('location-nav'),
+    elButtons: [],
+    locations: [ 'PDX Airport',
+                 'Pioneer Square', 
+                 'Powell\'s', 
+                 'St. John\'s', 
+                 'Waterfront' ],
+    maps: [],
+
+    createElButtons: function() {
+        for(var i = 0; i < this.locations.length; i++) {
+            this.elButtons[i] = document.createElement('button');
+            this.elButtons[i].innerText = this.locations[i];
+            this.elNav.appendChild(this.elButtons[i]);
+        }
+    },
+}
+
+mapBank.createElButtons();
