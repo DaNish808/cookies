aa0f3ea3dcb39a2e6bf34e269deb75a55abaabef
David Nishikawa <triptype@gmail.com>
2017-08-11 23:58:15 -0700

    added dynamic content to locations page, along with minor styling; added another fish to index

--- maps.js
+++ maps.js
@@ -5,26 +5,33 @@ var mapBank = {
     elNav: document.getElementById('location-nav'),
     elButtons: [],
     elMapDiv: document.getElementById('map-container'),
+    elInfoDiv: document.getElementById('store_info'),
+    elImageDiv: document.getElementById('img_div'),
+
+
     locations: [ 'PDX Airport',
                  'Pioneer Square', 
                  'Powell\'s', 
                  'St. John\'s', 
                  'Waterfront' ],
+
     maps: [ '<div class="mapouter"><div class="gmap_canvas"><iframe width="2000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=10280%20NE%20Cascades%20Pkwy%2C%20Portland%2C%20OR%2097220&t=&z=12&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>embed google map <a href="http://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{overflow:hidden;height:500px;width:2000px;}.gmap_canvas {background:none!important;height:500px;width:2000px;}</style></div>',
             '<div class="mapouter"><div class="gmap_canvas"><iframe width="2000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=pioneer%20square%2C%20Portland%2C%20OR%2097220&t=&z=16&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>embed google map <a href="http://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{overflow:hidden;height:500px;width:2000px;}.gmap_canvas {background:none!important;height:500px;width:2000px;}</style></div>',
             '<div class="mapouter"><div class="gmap_canvas"><iframe width="2000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=pdx%20codefellows%2C%20Portland%2C%20OR%2097220&t=&z=16&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>embed google map <a href="http://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{overflow:hidden;height:500px;width:2000px;}.gmap_canvas {background:none!important;height:500px;width:2000px;}</style></div>',
             '<div class="mapouter"><div class="gmap_canvas"><iframe width="2000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=6635%20N%20Baltimore%20Ave%20%23102%2C%20Portland%2C%20OR%2097203&t=&z=14&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>embed google map <a href="http://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{overflow:hidden;height:500px;width:2000px;}.gmap_canvas {background:none!important;height:500px;width:2000px;}</style></div>',
             '<div class="mapouter"><div class="gmap_canvas"><iframe width="2000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=W%20Burnside%20St%20%26%20Ankeny%2C%20Portland%2C%20OR%2097204&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>embed google map <a href="http://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{overflow:hidden;height:500px;width:2000px;}.gmap_canvas {background:none!important;height:500px;width:2000px;}</style></div>' ],
-    info: [ '',
-            '',
-            '',
-            '',
-            '' ],
-    images: [ './pictures/frosted-cookie.jpg',
-              './pictures/shirt.jpg',
-              './pictures/',
-              './pictures/cutter.jpeg',
-              './pictures/' ],
+
+    info: [ '<h4>PDX Airport Location</h4><p>Come for our exquisitely decorated Salmon Cookies!</p><p>Address: 298323 NE Cascades Pkwy<br/>Portland, OR 97220</p><p>Phone: (555) 808-4383</p><p>Hours: 6:00am-8:00pm any day of the week</p>',
+            '<h4>On Pioneer Square</h4><p>Shirts on sale, only 1e9 left!</p><p>Address: 700 SW 5th Ave<br/>Portland, OR 97220</p><p>Phone: (555) 808-2930</p><p>Hours: 6:00am-8:00pm any day of the week</p>',
+            '<h4>Right next to Powell\'s</h4><p>All cookies made by these guys!</p><p>Address: 5.7 NW 10th Ave<br/>Portland, OR 97220</p><p>Phone: (555) 808-4383</p><p>Hours: 6:00am-8:00pm any day of the week</p>',
+            '<h4>Just off the St. John Bridge</h4><p>Clearance Sale: all inventory must go!</p><p>Address: Willamette River off Cathedral Park<br/>Portland, OR 97220</p><p>Phone: (555) 808-8008</p><p>Hours: 6:00am-8:00pm any day of the week</p>',
+            '<h4>On the Waterfront</h4><p>Our cookies are so good, they don\'t even look real!</p><p>Address: Tunnels beneath Skidmore Fountain<br/>Portland, OR 97220</p><p>Phone: look for the tin can on the string (hint: stormdrain)</p><p>Hours: 6:00am-8:00pm any day of the week</p>' ],
+            
+    images: [ 'frosted-cookie.jpg',
+              'shirt.jpg',
+              'family.jpg',
+              'cutter.jpeg',
+              'fish.jpg' ],
 
 
     createElButtons: function() {
@@ -36,28 +43,16 @@ var mapBank = {
     },
 
     displayMap: function(index) {
-        this.elMapDiv.innerHTML = '';
-        // throw new Error('Google Map Error');
         this.elMapDiv.innerHTML = this.maps[index];
     },
 
-    // getEventIndex: function(e) {
-    //     var i = 0;
-    //     var el = '';
-    //     while(e.previousSibling !== null) {
-    //         el = e.previousSibling;
-    //         i++;
-    //     }
-    //     return i;
-    // },
+    displayInfo: function(index) {
+        this.elInfoDiv.innerHTML = this.info[index];
+    },
 
-    // createListener: function() {
-    //     var checkE = 0;
-    //     this.elNav.addEventListener('click', this.displayMap( checkE = function(e) {
-    //         this.getEventIndex(e);
-    //     } ), false);
-    //     console.log(checkE);
-    // },
+    displayImage: function(index) {
+        this.elImageDiv.style.backgroundImage = 'url(' +this.images[index]+ ')';
+    },
 
     onEvent: function(event) {
         console.log('event: ' + i);
@@ -81,18 +76,18 @@ var mapBank = {
         this.elButtons[i].addEventListener('click', function() {
             event.preventDefault();
             mapBank.displayMap(i);
-
+            mapBank.displayInfo(i);
+            mapBank.displayImage(i);
         });
     }
 }
 
-mapBank.createElButtons();
 
+
+mapBank.createElButtons();
 mapBank.displayMap(0);
 
 
 for(var i = 0; i < mapBank.elButtons.length; i++){
     mapBank.createListener(i);
 }
-
-clear();
